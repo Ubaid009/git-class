a546c78365551cf5e1c00060572261dc52906fea
Ubaid009 <ubaidullahp009@gmil.com>
2023-06-24 22:02:20 +0500

    new Lines

---
+++ Assignment3.dart
@@ -0,0 +1,35 @@
+
+void.main() {
+
+    // Q.1: Create a list of names and print all names using list.
+
+List<dynamic> studentslist = ["Zahid", "Ubaid", "Ovais", "Adeel", "Amir",];
+print(studentslist);
+
+// Q.2: Create an empty list of type string called days. Use the add method to add names of 7 days and print all days.
+List<String> days = [];
+
+// List<dynamic> days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",];
+// print(days);
+days.add("Monday");
+days.add("Tuesday");
+days.add("Wednesday");
+days.add("Thursday");
+days.add("Friday");
+days.add("Saturday");
+days.add("Sunday");
+print(days);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
